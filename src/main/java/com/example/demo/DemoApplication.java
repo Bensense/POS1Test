3b5357ad77cdb7e6ac159274aa51c7f47bf47415
Bensense <43452220+Bensense@users.noreply.github.com>
2018-12-19 14:05:17 +0100

    ein Versuch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,49 +9,31 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
-    Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
+        Scanner scan = new Scanner(System.in);
 
         System.out.println();
         System.out.println("A Useless Calculator");
         System.out.println();
 
         System.out.print("1. Zahl eingeben: ");
-        int zahl1 = intEingabe();
+        int zahl1 = scan.nextInt();
 
         System.out.print("2. Zahl eingeben: ");
-        int zahl2 = intEingabe();
+        int zahl2 = scan.nextInt();
 
         int ergebnis = zahl1 + zahl2;
 
         System.out.println();
         System.out.println("Das Ergebnis ist: " + ergebnis);
-
     }
 
-    public int intEingabe() {
-        int eingabe = 0;
-//        System.out.print("Eingabe: ");
-        try
-        {
-            eingabe = scan.nextInt();
-        }
-        catch(InputMismatchException e)
-        {
-            scan.next();
-            System.out.println("Falsche Eingabe");
-            intEingabe();
 
-        }
-        return eingabe;
-    }
+
 }
 
